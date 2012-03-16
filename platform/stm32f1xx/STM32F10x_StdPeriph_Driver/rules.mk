7bf1b5067eec2e200e411f26b0638685e491692f
Travis Geiselbrecht <geist@foobox.com>
2012-03-16 14:46:55 -0700

    [stm32f1xx] add ST's standard peripheral driver code

---
+++ platform/stm32f1xx/STM32F10x_StdPeriph_Driver/rules.mk
@@ -0,0 +1,28 @@
+LOCAL_DIR := $(GET_LOCAL_DIR)
+
+INCLUDES += -I$(LOCAL_DIR)/inc
+
+OBJS += \
+	$(LOCAL_DIR)/src/misc.o \
+	$(LOCAL_DIR)/src/stm32f10x_adc.o \
+	$(LOCAL_DIR)/src/stm32f10x_bkp.o \
+	$(LOCAL_DIR)/src/stm32f10x_can.o \
+	$(LOCAL_DIR)/src/stm32f10x_cec.o \
+	$(LOCAL_DIR)/src/stm32f10x_crc.o \
+	$(LOCAL_DIR)/src/stm32f10x_dac.o \
+	$(LOCAL_DIR)/src/stm32f10x_dbgmcu.o \
+	$(LOCAL_DIR)/src/stm32f10x_dma.o \
+	$(LOCAL_DIR)/src/stm32f10x_exti.o \
+	$(LOCAL_DIR)/src/stm32f10x_flash.o \
+	$(LOCAL_DIR)/src/stm32f10x_fsmc.o \
+	$(LOCAL_DIR)/src/stm32f10x_gpio.o \
+	$(LOCAL_DIR)/src/stm32f10x_i2c.o \
+	$(LOCAL_DIR)/src/stm32f10x_iwdg.o \
+	$(LOCAL_DIR)/src/stm32f10x_pwr.o \
+	$(LOCAL_DIR)/src/stm32f10x_rcc.o \
+	$(LOCAL_DIR)/src/stm32f10x_rtc.o \
+	$(LOCAL_DIR)/src/stm32f10x_sdio.o \
+	$(LOCAL_DIR)/src/stm32f10x_spi.o \
+	$(LOCAL_DIR)/src/stm32f10x_tim.o \
+	$(LOCAL_DIR)/src/stm32f10x_usart.o \
+	$(LOCAL_DIR)/src/stm32f10x_wwdg.o
--- platform/stm32f1xx/rules.mk
+++ platform/stm32f1xx/rules.mk
@@ -3,10 +3,16 @@ LOCAL_DIR := $(GET_LOCAL_DIR)
 # ROMBASE, MEMBASE, and MEMSIZE are required for the linker script
 ROMBASE := 0x0
 MEMBASE := 0x20000000
-MEMSIZE := 65536
+# can be overridden by target
+MEMSIZE ?= 65536 
 
 ARCH := arm
 ARM_CPU := cortex-m3
+
+ifeq ($(STM32_CHIP),stm32f107)
+DEFINES += \
+	STM32F10X_CL=1	
+endif
 
 INCLUDES += \
 	-I$(LOCAL_DIR)/include
@@ -33,3 +39,4 @@ OBJS += \
 LINKER_SCRIPT += \
 	$(BUILDDIR)/system-twosegment.ld
 
+include $(LOCAL_DIR)/STM32F10x_StdPeriph_Driver/rules.mk $(LOCAL_DIR)/CMSIS/rules.mk
