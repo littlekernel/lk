c32135b7e5972ec77f0883f55dd8b866b3c07c0d
Travis Geiselbrecht <geist@foobox.com>
2012-06-27 19:35:34 -0700

    [app] add feature to set the stack size in the static descriptor

--- include/app.h
+++ include/app.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2009 Travis Geiselbrecht
+ * Copyright (c) 2009-2012 Travis Geiselbrecht
  *
  * Permission is hereby granted, free of charge, to any person obtaining
  * a copy of this software and associated documentation files
@@ -23,6 +23,8 @@
 #ifndef __APP_H
 #define __APP_H
 
+#include <sys/types.h>
+
 /* app support api */
 void apps_init(void); /* one time setup */
 
@@ -33,6 +35,7 @@ typedef void (*app_entry)(const struct app_descriptor *, void *args);
 
 /* app startup flags */
 #define APP_FLAG_DONT_START_ON_BOOT 0x1
+#define APP_FLAG_CUSTOM_STACK_SIZE 0x2
 
 /* each app needs to define one of these to define its startup conditions */
 struct app_descriptor {
@@ -40,6 +43,7 @@ struct app_descriptor {
 	app_init  init;
 	app_entry entry;
 	unsigned int flags;
+	size_t stack_size;
 };
 
 #define APP_START(appname) struct app_descriptor _app_##appname __SECTION(".apps") = { .name = #appname,
