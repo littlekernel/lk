6c8cc76e5951e6ab78f05894d5b286c8c9335998
Travis Geiselbrecht <geist@foobox.com>
2012-07-01 20:52:46 -0700

    [target][stm32-h103] initial port to Olimex stm32-h103

---
+++ project/stm32-h103-test.mk
@@ -0,0 +1,7 @@
+LOCAL_DIR := $(GET_LOCAL_DIR)
+
+TARGET := stm32-h103
+MODULES += \
+	app/tests \
+	app/shell
+
---
+++ target/stm32-h103/rules.mk
@@ -0,0 +1,11 @@
+LOCAL_DIR := $(GET_LOCAL_DIR)
+
+STM32_CHIP := stm32f103
+
+PLATFORM := stm32f1xx
+
+INCLUDES += -I$(LOCAL_DIR)/include
+
+OBJS += \
+	$(LOCAL_DIR)/init.o
+
