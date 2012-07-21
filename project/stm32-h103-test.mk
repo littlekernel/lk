50a541ae0c319443dd83469ec9a09c66a5716593
Travis Geiselbrecht <geist@foobox.com>
2012-07-21 13:48:28 -0700

    [project][stm32-*] add lib/debugcommands back to the dep list

--- project/stm32-h103-test.mk
+++ project/stm32-h103-test.mk
@@ -3,5 +3,6 @@ LOCAL_DIR := $(GET_LOCAL_DIR)
 TARGET := stm32-h103
 MODULES += \
 	app/tests \
-	app/shell
+	app/shell \
+	lib/debugcommands \
 
--- project/stm32-p107-test.mk
+++ project/stm32-p107-test.mk
@@ -2,6 +2,7 @@ LOCAL_DIR := $(GET_LOCAL_DIR)
 
 TARGET := stm32-p107
 MODULES += \
+	lib/debugcommands \
 	app/tests \
 	app/shell
 
