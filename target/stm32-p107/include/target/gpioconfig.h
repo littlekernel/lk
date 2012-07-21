356362e8ece5e54a32ad42043ebb330e65065002
Travis Geiselbrecht <geist@foobox.com>
2012-07-21 13:11:46 -0700

    [target][stm32-p107] switch to the new gpio api

--- target/stm32-p107/include/target/gpioconfig.h
+++ target/stm32-p107/include/target/gpioconfig.h
@@ -20,56 +20,12 @@
  * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
  * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
-#include <err.h>
-#include <debug.h>
-#include <target.h>
-#include <compiler.h>
-#include <dev/gpio.h>
-#include <stm32f10x_usart.h>
-#include <stm32f10x_rcc.h>
-#include <stm32f10x_gpio.h>
-#include <stm32f10x_flash.h>
-#include <stm32f10x_dbgmcu.h>
-#include <platform/stm32.h>
+#ifndef __TARGET_GPIOCONFIG_H
+#define __TARGET_GPIOCONFIG_H
+
 #include <platform/gpio.h>
-#include <target/gpioconfig.h>
 
-void target_early_init(void)
-{
-	/* configure the usart3 pins */
-	GPIO_PinRemapConfig(GPIO_FullRemap_USART3, ENABLE);
+#define GPIO_LED0 GPIO(GPIO_PORT_C, 6)
+#define GPIO_LED1 GPIO(GPIO_PORT_C, 7)
 
-	gpio_config(GPIO(GPIO_PORT_D, 8), GPIO_STM32_AF);
-	gpio_config(GPIO(GPIO_PORT_D, 9), GPIO_INPUT);
-
-	stm32_debug_early_init();
-
-	/* configure some status leds */
-	gpio_set(GPIO_LED0, 0);
-	gpio_set(GPIO_LED1, 0);
-
-	gpio_config(GPIO_LED0, GPIO_OUTPUT);
-	gpio_config(GPIO_LED1, GPIO_OUTPUT);
-}
-
-void target_init(void)
-{
-	TRACE_ENTRY;
-
-	stm32_debug_init();
-
-	TRACE_EXIT;
-}
-
-void target_set_debug_led(unsigned int led, bool on)
-{
-	switch (led) {
-		case 0:
-			gpio_set(GPIO_LED0, on);
-			break;
-		case 1:
-			gpio_set(GPIO_LED1, on);
-			break;
-	}
-}
-
+#endif
