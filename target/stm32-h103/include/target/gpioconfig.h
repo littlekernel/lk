001514abf703a3269aefeea9b73fb1d07a3ddd06
Travis Geiselbrecht <geist@foobox.com>
2012-07-21 13:36:43 -0700

    [target][stm32-h103] convert to the new gpio interface

--- target/stm32-h103/include/target/gpioconfig.h
+++ target/stm32-h103/include/target/gpioconfig.h
@@ -20,49 +20,11 @@
  * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
  * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
-#include <err.h>
-#include <debug.h>
-#include <target.h>
-#include <compiler.h>
-#include <dev/gpio.h>
+#ifndef __TARGET_GPIOCONFIG_H
+#define __TARGET_GPIOCONFIG_H
+
 #include <platform/gpio.h>
-#include <stm32f10x_usart.h>
-#include <stm32f10x_rcc.h>
-#include <stm32f10x_gpio.h>
-#include <stm32f10x_flash.h>
-#include <stm32f10x_dbgmcu.h>
-#include <platform/stm32.h>
-#include <target/gpioconfig.h>
 
-void target_early_init(void)
-{
-	/* configure the usart1 pins */
-	gpio_config(GPIO(GPIO_PORT_A, 9), GPIO_STM32_AF);
-	gpio_config(GPIO(GPIO_PORT_A, 10), GPIO_INPUT);
+#define GPIO_LED0 GPIO(GPIO_PORT_C, 12)
 
-	stm32_debug_early_init();
-
-	/* configure some status leds */
-	gpio_set(GPIO_LED0, 1);
-
-	gpio_config(GPIO_LED0, GPIO_OUTPUT);
-}
-
-void target_init(void)
-{
-	TRACE_ENTRY;
-
-	stm32_debug_init();
-
-	TRACE_EXIT;
-}
-
-void target_set_debug_led(unsigned int led, bool on)
-{
-	switch (led) {
-		case 0:
-			gpio_set(GPIO_LED0, !on);
-			break;
-	}
-}
-
+#endif
