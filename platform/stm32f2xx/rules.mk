d4acbaad0675a493e8caea17b8041d9650a97c2e
Chris Anderson <chris@nullcode.org>
2012-10-03 15:10:53 -0700

    Initial skeleton / build for stm32f2xx

---
+++ platform/stm32f2xx/rules.mk
@@ -0,0 +1,77 @@
+LOCAL_DIR := $(GET_LOCAL_DIR)
+
+MODULE := $(LOCAL_DIR)
+
+# ROMBASE, MEMBASE, and MEMSIZE are required for the linker script
+ROMBASE := 0x0
+MEMBASE := 0x20000000
+# can be overridden by target
+
+ARCH := arm
+ARM_CPU := cortex-m3
+
+#ifeq ($(STM32_CHIP),stm32f107)
+#DEFINES += \
+#	STM32F10X_CL=1	
+#MEMSIZE ?= 65536
+#endif
+#ifeq ($(STM32_CHIP),stm32f103_xl)
+#DEFINES += \
+#	STM32F10X_XL=1
+#MEMSIZE ?= 65536
+#endif
+#ifeq ($(STM32_CHIP),stm32f103_hd)
+#DEFINES += \
+#	STM32F10X_HD=1
+#MEMSIZE ?= 65536
+#endif
+#ifeq ($(STM32_CHIP),stm32f103_md)
+#DEFINES += \
+#	STM32F10X_MD=1
+#MEMSIZE ?= 20480
+#endif
+#ifeq ($(STM32_CHIP),stm32f103_ld)
+#DEFINES += \
+#	STM32F10X_LD=1
+#MEMSIZE ?= 20480
+#endif
+
+DEFINES += \
+	MEMSIZE=$(MEMSIZE)
+
+INCLUDES += \
+	-I$(LOCAL_DIR)/include
+
+MODULE_SRCS += \
+	$(LOCAL_DIR)/init.c \
+	$(LOCAL_DIR)/debug.c \
+	$(LOCAL_DIR)/uart.c \
+	$(LOCAL_DIR)/timer.c \
+	$(LOCAL_DIR)/vectab.c \
+	$(LOCAL_DIR)/gpio.c \
+	$(LOCAL_DIR)/flash_nor.c \
+
+#	$(LOCAL_DIR)/debug.c \
+	$(LOCAL_DIR)/interrupts.c \
+	$(LOCAL_DIR)/platform_early.c \
+	$(LOCAL_DIR)/platform.c \
+	$(LOCAL_DIR)/timer.c \
+	$(LOCAL_DIR)/init_clock.c \
+	$(LOCAL_DIR)/init_clock_48mhz.c \
+	$(LOCAL_DIR)/mux.c \
+	$(LOCAL_DIR)/emac_dev.c
+
+# use a two segment memory layout, where all of the read-only sections 
+# of the binary reside in rom, and the read/write are in memory. The 
+# ROMBASE, MEMBASE, and MEMSIZE make variables are required to be set 
+# for the linker script to be generated properly.
+#
+LINKER_SCRIPT += \
+	$(BUILDDIR)/system-twosegment.ld
+
+MODULE_DEPS += \
+	lib/cbuf
+
+include $(LOCAL_DIR)/STM32F2xx_StdPeriph_Driver/rules.mk $(LOCAL_DIR)/CMSIS/rules.mk
+
+include make/module.mk
