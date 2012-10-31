9202b462a1b96f744dd0f28e35a892a42b411a55
Travis Geiselbrecht <geist@foobox.com>
2012-10-30 21:07:10 -0700

    [sam7ex256] remove the at91sam7 and sam7ex256 platform and target
    The first port of lk, RIP.

--- project/sam7ex256-test.mk
+++
@@ -1,9 +0,0 @@
-# top level project rules for the sam7ex256-test project
-#
-LOCAL_DIR := $(GET_LOCAL_DIR)
-
-TARGET := sam7ex256
-MODULES += \
-	app/tests \
-	app/shell
-
--- target/sam7ex256/mkmux.sh
+++
@@ -1,1 +0,0 @@
-../../platform/at91sam7/mkboard.py ../../platform/at91sam7/at91sam7x.pins include/platform/mux.def
