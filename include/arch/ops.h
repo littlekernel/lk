771fa3f89196df2d7b870569e1e8deb95ada9099
Travis Geiselbrecht <geist@foobox.com>
2014-01-26 22:49:39 -0800

    [arch] move arch/ops.h to arch/arch_ops.h and clean up header

--- include/arch/ops.h
+++ include/arch/ops.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2008 Travis Geiselbrecht
+ * Copyright (c) 2008-2014 Travis Geiselbrecht
  *
  * Permission is hereby granted, free of charge, to any person obtaining
  * a copy of this software and associated documentation files
@@ -66,11 +66,6 @@ __END_CDECLS
 
 #endif // !ASSEMBLY
 
-#if ARCH_ARM
-#include <arch/arm/ops.h>
-#endif
-#if ARCH_X86
-#include <arch/x86/ops.h>
-#endif
+#include <arch/arch_ops.h>
 
 #endif
