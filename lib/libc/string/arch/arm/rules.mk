33b94d9b97ef835d68aca2e5edb54f7267c70625
Travis Geiselbrecht <geist@foobox.com>
2014-03-09 03:37:18 -0700

    [lib][libc][arm-m] add new arm-m optimized memcpy and memset routines

--- lib/libc/string/arch/arm/rules.mk
+++ lib/libc/string/arch/arm/rules.mk
@@ -5,10 +5,23 @@ ifeq ($(SUBARCH),arm)
 ASM_STRING_OPS := bcopy bzero memcpy memmove memset
 
 MODULE_SRCS += \
-	$(LOCAL_DIR)/memcpy.S \
-	$(LOCAL_DIR)/memset.S
+	$(LOCAL_DIR)/arm/memcpy.S \
+	$(LOCAL_DIR)/arm/memset.S
 
 # filter out the C implementation
 C_STRING_OPS := $(filter-out $(ASM_STRING_OPS),$(C_STRING_OPS))
 endif
+
+ifeq ($(SUBARCH),arm-m)
+
+ASM_STRING_OPS := bcopy bzero memcpy memset
+
+MODULE_SRCS += \
+	$(LOCAL_DIR)/arm-m/memcpy.S \
+	$(LOCAL_DIR)/arm-m/memset.S
+
+# filter out the C implementation
+C_STRING_OPS := $(filter-out $(ASM_STRING_OPS),$(C_STRING_OPS))
+endif
+
 
