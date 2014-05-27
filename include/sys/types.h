c608b945dd610c2a5ad0ec664b0a89c630b33093
Brian Swetland <swetland@frotz.net>
2014-05-26 22:07:20 -0700

    [include][sys] add shorthand integer types
    u8/u16/u32/u64 and s8/s16/s32/s64 for those who prefer minimum verbosity

--- include/sys/types.h
+++ include/sys/types.h
@@ -62,4 +62,14 @@ enum handler_return {
 
 typedef signed long int ssize_t;
 
+typedef uint8_t u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
+
+typedef int8_t s8;
+typedef int16_t s16;
+typedef int32_t s32;
+typedef int64_t s64;
+
 #endif
