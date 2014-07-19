e0f001dcfb50fd68540337b0be5ef184976ae412
Travis Geiselbrecht <geist@foobox.com>
2014-07-19 15:42:28 -0700

    [lib][iovec] some iovec utils

---
+++ lib/iovec/rules.mk
@@ -0,0 +1,12 @@
+LOCAL_DIR := $(GET_LOCAL_DIR)
+
+MODULE := $(LOCAL_DIR)
+
+GLOBAL_INCLUDES += $(LOCAL_DIR)/include
+
+MODULE_DEPS := \
+
+MODULE_SRCS := \
+	$(LOCAL_DIR)/iovec.c \
+
+include make/module.mk
