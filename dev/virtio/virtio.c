b1ccb192cf68a9afb838b59c1aabcc41d71b8668
Travis Geiselbrecht <geist@foobox.com>
2014-08-18 14:55:53 -0700

    [dev][virtio] update memory allocations to match VM alignment api

--- dev/virtio/virtio.c
+++ dev/virtio/virtio.c
@@ -264,7 +264,7 @@ status_t virtio_alloc_ring(struct virtio_device *dev, uint index, uint16_t len)
 
 #if WITH_KERNEL_VM
     void *vptr;
-    status_t err = vmm_alloc_contiguous(vmm_get_kernel_aspace(), "virtio_ring", size, &vptr, 0, ARCH_MMU_FLAG_UNCACHED_DEVICE);
+    status_t err = vmm_alloc_contiguous(vmm_get_kernel_aspace(), "virtio_ring", size, &vptr, 0, 0, ARCH_MMU_FLAG_UNCACHED_DEVICE);
     if (err < 0)
         return ERR_NO_MEMORY;
 
