f98d8a275551a5bf84bb133998bacf89ea5a6dc6
Sergio Rodriguez <sergio.sf.rodriguez@intel.com>
2014-08-28 16:23:16 -0700

    Modifying console for x86-64

--- project/pc-x86-64-test.mk
+++ project/pc-x86-64-test.mk
@@ -7,13 +7,5 @@ TARGET := pc-x86
 MODULES += \
 	app/tests \
 	app/shell
-#	app/pcitests
 
 
-# extra rules to copy the pc-x86.conf file to the build dir
-#$(BUILDDIR)/pc-x86.conf: $(LOCAL_DIR)/pc-x86.conf
-#	@echo copy $< to $@
-#	$(NOECHO)cp $< $@
-
-#EXTRA_BUILDDEPS += $(BUILDDIR)/pc-x86.conf
-#GENERATED += $(BUILDDIR)/pc-x86.conf
