0899bc7cfd070cf6c09bcda68a2f94318623e7b4
Travis Geiselbrecht <geist@foobox.com>
2014-11-24 17:46:18 -0800

    [lib][bootimage] add a few accessors for the bootimage library

--- lib/bootimage/include/lib/bootimage.h
+++ lib/bootimage/include/lib/bootimage.h
@@ -30,7 +30,8 @@ typedef struct bootimage bootimage_t;
 
 status_t bootimage_open(const void *ptr, size_t len, bootimage_t **bi) __NONNULL();
 status_t bootimage_close(bootimage_t *bi) __NONNULL();
+status_t bootimage_get_range(bootimage_t *bi, const void **ptr, size_t *len) __NONNULL((1));
 
 /* ask for a file section of the bootimage, by type */
-status_t bootimage_get_file_section(bootimage_t *bi, uint32_t type, void **ptr, size_t *len) __NONNULL((1));
+status_t bootimage_get_file_section(bootimage_t *bi, uint32_t type, const void **ptr, size_t *len) __NONNULL((1));
 
