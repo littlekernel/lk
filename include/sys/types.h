cbe13735ca457dbc6eca0d4903a2bfecac7354bf
Arve Hjønnevåg <arve@android.com>
2014-12-05 20:00:57 -0800

    [include][sys][types.h] Change lk_time_t to uint32_t
    Use a fixed size for lk_time_t so the same time conversions can
    be used on 64 bit and 32 bit systems.
    
    Change-Id: I92ce900fc6b3d783a7d93a05c2ecf8fb236552ad

--- include/sys/types.h
+++ include/sys/types.h
@@ -46,14 +46,14 @@ typedef uintptr_t paddr_t;
 
 typedef int kobj_id;
 
-typedef unsigned long lk_time_t;
+typedef uint32_t lk_time_t;
 typedef unsigned long long lk_bigtime_t;
-#define INFINITE_TIME ULONG_MAX
+#define INFINITE_TIME UINT32_MAX
 
-#define TIME_GTE(a, b) ((long)((a) - (b)) >= 0)
-#define TIME_LTE(a, b) ((long)((a) - (b)) <= 0)
-#define TIME_GT(a, b) ((long)((a) - (b)) > 0)
-#define TIME_LT(a, b) ((long)((a) - (b)) < 0)
+#define TIME_GTE(a, b) ((int32_t)((a) - (b)) >= 0)
+#define TIME_LTE(a, b) ((int32_t)((a) - (b)) <= 0)
+#define TIME_GT(a, b) ((int32_t)((a) - (b)) > 0)
+#define TIME_LT(a, b) ((int32_t)((a) - (b)) < 0)
 
 enum handler_return {
     INT_NO_RESCHEDULE = 0,
