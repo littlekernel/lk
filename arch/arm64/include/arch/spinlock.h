97e3611e1d99aad75f63f214afce294a60976a95
Arve Hjønnevåg <arve@android.com>
2015-01-30 16:34:02 -0800

    [arch][arm64] Implement arch_interrupt_save/restore flags w/FIQ support
    Change-Id: I4292e6a882a1fb3d9cf78abee394f159756ab340

--- arch/arm64/include/arch/spinlock.h
+++ arch/arm64/include/arch/spinlock.h
@@ -63,21 +63,42 @@ static inline bool arch_spin_lock_held(spin_lock_t *lock)
     return *lock != 0;
 }
 
+enum {
+    /* Possible future flags:
+     * SPIN_LOCK_FLAG_PMR_MASK         = 0x000000ff,
+     * SPIN_LOCK_FLAG_PREEMPTION       = 0x10000000,
+     * SPIN_LOCK_FLAG_SET_PMR          = 0x20000000,
+     */
+
+    /* ARM specific flags */
+    SPIN_LOCK_FLAG_IRQ              = 0x40000000,
+    SPIN_LOCK_FLAG_FIQ              = 0x80000000, /* Do not use unless IRQs are already disabled */
+    SPIN_LOCK_FLAG_IRQ_FIQ          = SPIN_LOCK_FLAG_IRQ | SPIN_LOCK_FLAG_FIQ,
+
+    /* Generic flags */
+    SPIN_LOCK_FLAG_INTERRUPTS       = SPIN_LOCK_FLAG_IRQ,
+};
+
     /* default arm flag is to just disable plain irqs */
-#define ARCH_DEFAULT_SPIN_LOCK_FLAG_INTERRUPTS  0
+#define ARCH_DEFAULT_SPIN_LOCK_FLAG_INTERRUPTS  SPIN_LOCK_FLAG_INTERRUPTS
 
 enum {
     /* private */
     SPIN_LOCK_STATE_RESTORE_IRQ = 1,
+    SPIN_LOCK_STATE_RESTORE_FIQ = 2,
 };
 
 static inline void
 arch_interrupt_save(spin_lock_saved_state_t *statep, spin_lock_save_flags_t flags)
 {
     spin_lock_saved_state_t state = 0;
-    if (!arch_ints_disabled()) {
+    if ((flags & SPIN_LOCK_FLAG_IRQ) && !arch_ints_disabled()) {
         state |= SPIN_LOCK_STATE_RESTORE_IRQ;
         arch_disable_ints();
+    }
+    if ((flags & SPIN_LOCK_FLAG_FIQ) && !arch_fiqs_disabled()) {
+        state |= SPIN_LOCK_STATE_RESTORE_FIQ;
+        arch_disable_fiqs();
     }
     *statep = state;
 }
@@ -85,7 +106,9 @@ arch_interrupt_save(spin_lock_saved_state_t *statep, spin_lock_save_flags_t flag
 static inline void
 arch_interrupt_restore(spin_lock_saved_state_t old_state, spin_lock_save_flags_t flags)
 {
-    if (old_state & SPIN_LOCK_STATE_RESTORE_IRQ)
+    if ((flags & SPIN_LOCK_FLAG_FIQ) && (old_state & SPIN_LOCK_STATE_RESTORE_FIQ))
+        arch_enable_fiqs();
+    if ((flags & SPIN_LOCK_FLAG_IRQ) && (old_state & SPIN_LOCK_STATE_RESTORE_IRQ))
         arch_enable_ints();
 }
 
