24e17274c7fd63237d0a85ca53c16bd4b0ec9366
Travis Geiselbrecht <geist@foobox.com>
2015-02-21 20:08:51 -0800

    [arch][arm] without SMP, emit nulled out versions of spinlock routines

--- arch/arm/include/arch/spinlock.h
+++ arch/arm/include/arch/spinlock.h
@@ -32,10 +32,6 @@ typedef unsigned long spin_lock_t;
 typedef unsigned long spin_lock_saved_state_t;
 typedef unsigned long spin_lock_save_flags_t;
 
-void arch_spin_lock(spin_lock_t *lock);
-int arch_spin_trylock(spin_lock_t *lock);
-void arch_spin_unlock(spin_lock_t *lock);
-
 static inline void arch_spin_lock_init(spin_lock_t *lock)
 {
     *lock = SPIN_LOCK_INITIAL_VALUE;
@@ -45,6 +41,31 @@ static inline bool arch_spin_lock_held(spin_lock_t *lock)
 {
     return *lock != 0;
 }
+
+#if WITH_SMP
+
+void arch_spin_lock(spin_lock_t *lock);
+int arch_spin_trylock(spin_lock_t *lock);
+void arch_spin_unlock(spin_lock_t *lock);
+
+#else
+
+static inline void arch_spin_lock(spin_lock_t *lock)
+{
+    *lock = 1;
+}
+
+static inline int arch_spin_trylock(spin_lock_t *lock)
+{
+    return 0;
+}
+
+static inline void arch_spin_unlock(spin_lock_t *lock)
+{
+    *lock = 0;
+}
+
+#endif
 
     /* ARM specific flags */
 #define SPIN_LOCK_FLAG_IRQ                      0x40000000
