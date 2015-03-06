9dd2d181711c79da4603986c117b999c4a497e62
Stefan Kristiansson <stefan.kristiansson@saunalahti.fi>
2015-03-06 18:52:38 +0200

    [platform][or1ksim] add or1ksim platform for the or1k architecture
    or1ksim is the "golden reference" simulator for the or1k
    architecture, but the configuration used in this simple setup
    is commonly used on other targets, so it could serve as a good
    starting point for those as well.
    
    Included is also a configuration file with uart and ethernet
    peripherals defined, which of only the uart is supported.

---
+++ platform/or1ksim/rules.mk
@@ -0,0 +1,31 @@
+LOCAL_DIR := $(GET_LOCAL_DIR)
+
+MODULE := $(LOCAL_DIR)
+
+ARCH := or1k
+
+MODULE_DEPS += \
+	lib/cbuf \
+	dev/interrupt/or1k_pic \
+	dev/timer/or1k_ticktimer
+
+GLOBAL_INCLUDES += \
+	$(LOCAL_DIR)/include
+
+MODULE_SRCS += \
+	$(LOCAL_DIR)/platform.c \
+	$(LOCAL_DIR)/uart.c
+
+MEMBASE ?= 0x00000000
+MEMSIZE ?= 0x02000000
+
+# we have an mmu
+WITH_KERNEL_VM=1
+
+KERNEL_BASE = 0xc0000000
+
+GLOBAL_DEFINES += \
+	MEMBASE=$(MEMBASE) \
+	MEMSIZE=$(MEMSIZE)
+
+include make/module.mk
