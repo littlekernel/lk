ab3e0c147a6acad27ea0e5aefbade9246da3c00b
Travis Geiselbrecht <geist@foobox.com>
2015-04-18 14:26:14 -0700

    [app][tests] add a instruction multi issue test, clean up formatting

--- app/tests/rules.mk
+++ app/tests/rules.mk
@@ -5,17 +5,19 @@ MODULE := $(LOCAL_DIR)
 GLOBAL_INCLUDES += $(LOCAL_DIR)/include
 
 MODULE_SRCS += \
-	$(LOCAL_DIR)/tests.c \
-	$(LOCAL_DIR)/thread_tests.c \
-	$(LOCAL_DIR)/printf_tests.c \
-	$(LOCAL_DIR)/clock_tests.c \
-	$(LOCAL_DIR)/cache_tests.c \
-	$(LOCAL_DIR)/benchmarks.c \
-	$(LOCAL_DIR)/float.c \
-	$(LOCAL_DIR)/float_instructions.S \
-	$(LOCAL_DIR)/float_test_vec.c \
-	$(LOCAL_DIR)/fibo.c \
-	$(LOCAL_DIR)/mem_tests.c \
+    $(LOCAL_DIR)/benchmarks.c \
+    $(LOCAL_DIR)/cache_tests.c \
+    $(LOCAL_DIR)/clock_tests.c \
+    $(LOCAL_DIR)/fibo.c \
+    $(LOCAL_DIR)/float.c \
+    $(LOCAL_DIR)/float_instructions.S \
+    $(LOCAL_DIR)/float_test_vec.c \
+    $(LOCAL_DIR)/mem_tests.c \
+    $(LOCAL_DIR)/printf_tests.c \
+    $(LOCAL_DIR)/tests.c \
+    $(LOCAL_DIR)/thread_tests.c \
+
+MODULE_ARM_OVERRIDE_SRCS := \
 
 MODULE_COMPILEFLAGS += -Wno-format
 
