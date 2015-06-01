f76c4a4541fbdded6216e99909322330a21397fd
Travis Geiselbrecht <geist@foobox.com>
2015-06-01 15:29:06 -0700

    [project][uzed-bootloader] disable SMP for this project
    chainloading from SMP environment currently doesn't work properly.
    
    Change-Id: Iaed1fc92f0269c38522a420be972d957a13aebc4

--- project/uzed-bootloader.mk
+++ project/uzed-bootloader.mk
@@ -8,5 +8,8 @@ MODULES += \
 GLOBAL_DEFINES += \
 	SYSPARAM_ALLOW_WRITE=1
 
+# disable SMP for this build, chain loading from SMP currently does not work
+WITH_SMP:=0
+
 include project/target/uzed.mk
 
