1c3205eab1dd8912dff4ed30cf2827768b54d68b
Brian Swetland <swetland@frotz.net>
2015-06-30 23:03:02 -0700

    [target][stm32f4-discovery] update to use platform/stm32f4xx

--- target/stm32f4-discovery/init.c
+++ target/stm32f4-discovery/init.c
@@ -26,9 +26,9 @@
 #include <target.h>
 #include <compiler.h>
 #include <dev/gpio.h>
-#include <stm32f2xx_usart.h>
-#include <stm32f2xx_rcc.h>
-#include <stm32f2xx_gpio.h>
+#include <stm32f4xx_usart.h>
+#include <stm32f4xx_rcc.h>
+#include <stm32f4xx_gpio.h>
 #include <platform/stm32.h>
 #include <platform/gpio.h>
 #include <target/debugconfig.h>
@@ -38,12 +38,10 @@ void target_early_init(void)
 {
 #ifdef DEBUG_UART
 #if DEBUG_UART == 2
-	gpio_config(GPIO_USART2_TX, GPIO_STM32_AF | \
-                                    GPIO_STM32_AFn(GPIO_AF_USART2) | \
-                                    GPIO_PULLUP);
-	gpio_config(GPIO_USART2_RX, GPIO_STM32_AF | \
-                                    GPIO_STM32_AFn(GPIO_AF_USART2) | \
-                                    GPIO_PULLUP);
+	gpio_config(GPIO_USART2_TX, GPIO_STM32_AF |
+		GPIO_STM32_AFn(GPIO_AF_USART2) | GPIO_PULLUP);
+	gpio_config(GPIO_USART2_RX, GPIO_STM32_AF |
+		GPIO_STM32_AFn(GPIO_AF_USART2) | GPIO_PULLUP);
 #endif // DEBUG_UART == 2
 #else
 #warn DEBUG_UART only supports USART2!!!
@@ -70,18 +68,17 @@ void target_init(void)
 void target_set_debug_led(unsigned int led, bool on)
 {
 	switch (led) {
-		case 0:
-			gpio_set(GPIO_LED0, on);
-			break;
-		case 1:
-			gpio_set(GPIO_LED1, on);
-			break;
-		case 2:
-			gpio_set(GPIO_LED2, on);
-			break;
-		case 3:
-			gpio_set(GPIO_LED3, on);
-			break;
+	case 0:
+		gpio_set(GPIO_LED0, on);
+		break;
+	case 1:
+		gpio_set(GPIO_LED1, on);
+		break;
+	case 2:
+		gpio_set(GPIO_LED2, on);
+		break;
+	case 3:
+		gpio_set(GPIO_LED3, on);
+		break;
 	}
 }
-
