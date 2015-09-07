5e42748ad9d02cb9752b9136b8f2908d84c99ade
Travis Geiselbrecht <geist@foobox.com>
2015-09-06 17:54:48 -0700

    [platform][vexpress-a9] fix uart to handle overruns
    -Mask off the rx interrupt when the circular buffer fills up.

--- platform/vexpress-a9/uart.c
+++ platform/vexpress-a9/uart.c
@@ -109,11 +109,16 @@ static enum handler_return uart_irq(void *arg)
     uint32_t isr = UARTREG(base, UART_TMIS);
 
     if (isr & (1<<4)) { // rxmis
-        UARTREG(base, UART_ICR) = (1<<4);
         cbuf_t *rxbuf = &uart_rx_buf[port];
 
         /* while fifo is not empty, read chars out of it */
         while ((UARTREG(base, UART_TFR) & (1<<4)) == 0) {
+            /* if we're out of rx buffer, mask the irq instead of handling it */
+            if (cbuf_space_avail(rxbuf) == 0) {
+                UARTREG(base, UART_IMSC) &= ~(1<<4); // !rxim
+                break;
+            }
+
             char c = UARTREG(base, UART_DR);
             cbuf_write_char(rxbuf, c, false);
 
@@ -127,6 +132,8 @@ static enum handler_return uart_irq(void *arg)
 void uart_init(void)
 {
     for (size_t i = 0; i < NUM_UART; i++) {
+        uintptr_t base = uart_to_ptr(i);
+
         // create circular buffer to hold received data
         cbuf_initialize(&uart_rx_buf[i], RXBUF_SIZE);
 
@@ -134,16 +141,16 @@ void uart_init(void)
         register_int_handler(UART0_INT + i, &uart_irq, (void *)i);
 
         // clear all irqs
-        UARTREG(uart_to_ptr(i), UART_ICR) = 0x3ff;
+        UARTREG(base, UART_ICR) = 0x3ff;
 
         // set fifo trigger level
-        UARTREG(uart_to_ptr(i), UART_IFLS) = 0; // 1/8 rxfifo, 1/8 txfifo
+        UARTREG(base, UART_IFLS) = 0; // 1/8 rxfifo, 1/8 txfifo
 
         // enable rx interrupt
-        UARTREG(uart_to_ptr(i), UART_IMSC) = (1<<4); // rxim
+        UARTREG(base, UART_IMSC) = (1<<4); // rxim
 
         // enable receive
-        UARTREG(uart_to_ptr(i), UART_CR) |= (1<<9); // rxen
+        UARTREG(base, UART_CR) |= (1<<9); // rxen
 
         // enable interrupt
         unmask_interrupt(UART0_INT + i);
@@ -174,8 +181,10 @@ int uart_getc(int port, bool wait)
     cbuf_t *rxbuf = &uart_rx_buf[port];
 
     char c;
-    if (cbuf_read_char(rxbuf, &c, wait) == 1)
+    if (cbuf_read_char(rxbuf, &c, wait) == 1) {
+        UARTREG(uart_to_ptr(port), UART_IMSC) = (1<<4); // rxim
         return c;
+    }
 
     return -1;
 }
