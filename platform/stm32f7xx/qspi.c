587c3f93b2a9be8ed64968522cd0f2f62798317b
Gurjant Kalsi <me@gurjantkalsi.com>
2015-10-30 15:22:09 -0700

    [qspi][ioctl] qspiflash ioctl get mem map returns pointer to spiflash mem map base

--- platform/stm32f7xx/qspi.c
+++ platform/stm32f7xx/qspi.c
@@ -389,6 +389,10 @@ static int spiflash_ioctl(struct bdev* device, int request, void* argp)
         case BIO_IOCTL_GET_MEM_MAP:
             /* put the device into linear mode */
             ret = qspi_enable_linear();
+            if (ret != NO_ERROR)
+                break;
+            if (argp)
+                *(void **)argp = (void*)QSPI_BASE;
             break;
     }
 
