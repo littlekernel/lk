cf67ac37c2ce853ec90187f286b24fd2abacdf47
Carlos Pizano <cpu@chromium.org>
2015-12-03 14:19:31 -0800

    [lib][tftp] add CDECL to header.
    BUG=
    R=travisg@google.com
    
    Review URL: https://codereview.chromium.org/1490333006 .

--- lib/tftp/include/lib/tftp.h
+++ lib/tftp/include/lib/tftp.h
@@ -23,9 +23,14 @@
 
 #pragma once
 
+#include <compiler.h>
+
+__BEGIN_CDECLS
+
 typedef int (*tftp_callback_t)(void *data, size_t len, void *arg);
 
 int tftp_server_init(void *arg);
 
 int tftp_set_write_client(const char* file_name, tftp_callback_t cb, void* arg);
 
+__END_CDECLS
