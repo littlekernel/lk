efbd6b18929af870714814dc0850cb8a1ebfd306
Travis Geiselbrecht <geist@foobox.com>
2015-12-11 21:13:31 -0800

    [lib][cbuf] C++ header guard the cbuf header

--- lib/cbuf/include/lib/cbuf.h
+++ lib/cbuf/include/lib/cbuf.h
@@ -22,10 +22,13 @@
  */
 #pragma once
 
+#include <compiler.h>
 #include <sys/types.h>
 #include <kernel/event.h>
 #include <kernel/spinlock.h>
 #include <iovec.h>
+
+__BEGIN_CDECLS
 
 typedef struct cbuf {
     uint head;
@@ -156,4 +159,6 @@ static inline void cbuf_reset(cbuf_t *cbuf)
 /* special cases for dealing with a single char of data */
 size_t cbuf_read_char(cbuf_t *cbuf, char *c, bool block);
 size_t cbuf_write_char(cbuf_t *cbuf, char c, bool canreschedule);
+
+__END_CDECLS
 
