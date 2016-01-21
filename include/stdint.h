e4fdff3c7d16959f14bee964b068766f75609194
Michael Ryleev <gmar@google.com>
2016-01-21 09:54:37 -0800

    [include][stdint.h] Add definition of INTN_C and  UINTN_C macros
     INT8_C, INT16_C, INT32_C, INT64_C, INTMAX_C
     UINT8_C, UINT16_C  UINT32_C, UINT64_C and UINTMAX_C
     variants are supported.
    
    Change-Id: I3e94d0c93db51afe5633180d2520c0c2b99c63dc

--- include/stdint.h
+++ include/stdint.h
@@ -126,5 +126,18 @@ typedef unsigned long long uintmax_t;
 
 #define SIZE_MAX ULONG_MAX
 
+#define INT8_C(c)         (c)
+#define INT16_C(c)        (c)
+#define INT32_C(c)        (c)
+#define INT64_C(c)        (c ## LL)
+
+#define UINT8_C(c)        (c)
+#define UINT16_C(c)       (c)
+#define UINT32_C(c)       (c ## U)
+#define UINT64_C(c)       (c ## ULL)
+
+#define INTMAX_C(c)       INT64_C(c)
+#define UINTMAX_C(c)      UINT64_C(c)
+
 #endif
 
