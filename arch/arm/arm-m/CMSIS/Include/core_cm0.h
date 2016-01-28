056d6aada3f90644d171eb8c4a7b4e9e63366b2e
Gurjant Kalsi <me@gurjantkalsi.com>
2016-01-28 12:46:29 -0800

    [arm-m][irq][systick] Don't change the systick priority inside arm/systick. All interrupts should be the same priority to avoid nested interrupts which are unsupported by LK.

--- arch/arm/arm-m/CMSIS/Include/core_cm0.h
+++ arch/arm/arm-m/CMSIS/Include/core_cm0.h
@@ -716,7 +716,6 @@ __STATIC_INLINE uint32_t SysTick_Config(uint32_t ticks)
   if ((ticks - 1UL) > SysTick_LOAD_RELOAD_Msk) { return (1UL); }    /* Reload value impossible */
 
   SysTick->LOAD  = (uint32_t)(ticks - 1UL);                         /* set reload register */
-  NVIC_SetPriority (SysTick_IRQn, (1UL << __NVIC_PRIO_BITS) - 1UL); /* set Priority for Systick Interrupt */
   SysTick->VAL   = 0UL;                                             /* Load the SysTick Counter Value */
   SysTick->CTRL  = SysTick_CTRL_CLKSOURCE_Msk |
                    SysTick_CTRL_TICKINT_Msk   |
