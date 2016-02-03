e37f53d802177c9ebe77279c562a676f70c5bd70
Erik Gilling <konkers@konkers.net>
2016-02-03 08:36:20 -0800

    stm32f072-nucleo: Initial target support.
    Debug LED and uart works.

---
+++ target/nucleo-f072rb/rules.mk
@@ -0,0 +1,21 @@
+LOCAL_DIR := $(GET_LOCAL_DIR)
+
+MODULE := $(LOCAL_DIR)
+
+STM32_CHIP := stm32f072_xB
+
+PLATFORM := stm32f0xx
+
+GLOBAL_DEFINES += \
+	ENABLE_UART2=1 \
+	TARGET_HAS_DEBUG_LED=1 \
+	HSE_VALUE=8000000 \
+	PLL_M_VALUE=8 \
+	PLL_N_VALUE=336 \
+	PLL_P_VALUE=2
+
+MODULE_SRCS += \
+	$(LOCAL_DIR)/init.c
+
+include make/module.mk
+
