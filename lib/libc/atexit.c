3f7f7a96290727da06876745d11fa83d0f6ab1df
Travis Geiselbrecht <geist@foobox.com>
2016-02-19 12:23:18 -0800

    [c++] add some required arm symbols for c++

--- lib/libc/atexit.c
+++ lib/libc/atexit.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2008 Travis Geiselbrecht
+ * Copyright (c) 2008-2015 Travis Geiselbrecht
  *
  * Permission is hereby granted, free of charge, to any person obtaining
  * a copy of this software and associated documentation files
