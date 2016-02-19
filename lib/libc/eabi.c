ed19c37227fab12b3224a5dd24ccf8a2143eac16
Travis Geiselbrecht <geist@foobox.com>
2016-02-19 12:23:18 -0800

    [c++] add some required arm symbols for c++

--- lib/libc/eabi.c
+++ lib/libc/eabi.c
@@ -23,6 +23,8 @@
 /* some cruft we have to define when using the linux toolchain */
 #include <unwind.h>
 
+void *__dso_handle;
+
 #if defined(__ARM_EABI_UNWINDER__) && __ARM_EABI_UNWINDER__
 
 /* Our toolchain has eabi functionality built in, but they're not really used.
@@ -48,4 +50,13 @@ _Unwind_Reason_Code __aeabi_unwind_cpp_pr2(_Unwind_State state, _Unwind_Control_
 void raise(void)
 {
 }
+
+extern int __cxa_atexit(void (*func)(void *), void *arg, void *d);
+
+int __aeabi_atexit(void *arg, void (*func)(void *), void *d)
+{
+    return __cxa_atexit(func, arg, d);
+}
+
+
 
--- lib/libc/pure_virtual.cpp
+++ lib/libc/pure_virtual.cpp
@@ -27,3 +27,8 @@ extern "C" void __cxa_pure_virtual(void)
     panic("pure virtual called\n");
 }
 
+extern "C" int __cxa_atexit(void (*destructor)(void *), void *arg, void *__dso_handle)
+{
+    return 0;
+}
+
