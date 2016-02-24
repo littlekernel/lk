58dcee716bad0f93da3d181279488caf0e8c93e2
Travis Geiselbrecht <geist@foobox.com>
2016-02-23 21:05:59 -0800

    [project] rearrange the qemu-virt project files a bit

--- project/qemu-virt-a53-test.mk
+++ project/qemu-virt-a53-test.mk
@@ -1,14 +1,9 @@
 # main project for qemu-aarch64
-ARCH := arm64
-ARM_CPU := cortex-a53
-
 MODULES += \
 	app/shell
-
-WITH_LINKER_GC := 0
 
 include project/virtual/test.mk
 include project/virtual/fs.mk
 include project/virtual/minip.mk
-include project/target/qemu-virt.mk
+include project/target/qemu-virt-a53.mk
 
---
+++ project/target/qemu-virt-a15.mk
@@ -0,0 +1,6 @@
+# main project for qemu-arm32
+ARCH := arm
+ARM_CPU := cortex-a15
+
+include project/target/qemu-virt.mk
+
