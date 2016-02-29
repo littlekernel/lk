61b4f0450e54303176c3b0af71f1596fbe68559e
Gurjant Kalsi <me@gurjantkalsi.com>
2016-02-28 17:08:48 -0800

    [fs] Add a truncate file API to the FS api layer

--- lib/fs/include/lib/fs.h
+++ lib/fs/include/lib/fs.h
@@ -72,6 +72,7 @@ ssize_t fs_read_file(filehandle *handle, void *buf, off_t offset, size_t len) __
 ssize_t fs_write_file(filehandle *handle, const void *buf, off_t offset, size_t len) __NONNULL();
 status_t fs_close_file(filehandle *handle) __NONNULL();
 status_t fs_stat_file(filehandle *handle, struct file_stat *) __NONNULL((1));
+status_t fs_truncate_file(filehandle *handle, uint64_t len) __NONNULL((1));
 
 /* dir api */
 status_t fs_make_dir(const char *path) __NONNULL();
@@ -105,6 +106,7 @@ struct fs_api {
     status_t (*open)(fscookie *, const char *, filecookie **);
     status_t (*create)(fscookie *, const char *, filecookie **, uint64_t);
     status_t (*remove)(fscookie *, const char *);
+    status_t (*truncate)(filecookie *, uint64_t);
     status_t (*stat)(filecookie *, struct file_stat *);
     ssize_t (*read)(filecookie *, void *, off_t, size_t);
     ssize_t (*write)(filecookie *, const void *, off_t, size_t);
