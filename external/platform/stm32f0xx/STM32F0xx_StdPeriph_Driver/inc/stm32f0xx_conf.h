6f277bb95d7daad6ff7cf01a07ccc811e4f452b9
Erik Gilling <konkers@konkers.net>
2016-03-02 22:34:58 -0800

    [stm32f0xx] remove STM32F0xx_StdPeriph_Driver.

--- external/platform/stm32f0xx/STM32F0xx_StdPeriph_Driver/inc/stm32f0xx_conf.h
+++ external/platform/stm32f0xx/STM32F0xx_StdPeriph_Driver/inc/stm32f0xx_conf.h
@@ -31,29 +31,6 @@
 
 /* Includes ------------------------------------------------------------------*/
 /* Comment the line below to disable peripheral header file inclusion */
-#include "stm32f0xx_adc.h"
-#include "stm32f0xx_can.h"
-#include "stm32f0xx_cec.h"
-#include "stm32f0xx_crc.h"
-#include "stm32f0xx_crs.h"
-#include "stm32f0xx_comp.h"
-#include "stm32f0xx_dac.h"
-#include "stm32f0xx_dbgmcu.h"
-#include "stm32f0xx_dma.h"
-#include "stm32f0xx_exti.h"
-#include "stm32f0xx_flash.h"
-#include "stm32f0xx_gpio.h"
-#include "stm32f0xx_syscfg.h"
-#include "stm32f0xx_i2c.h"
-#include "stm32f0xx_iwdg.h"
-#include "stm32f0xx_pwr.h"
-#include "stm32f0xx_rcc.h"
-#include "stm32f0xx_rtc.h"
-#include "stm32f0xx_spi.h"
-#include "stm32f0xx_tim.h"
-#include "stm32f0xx_usart.h"
-#include "stm32f0xx_wwdg.h"
-#include "stm32f0xx_misc.h"  /* High level functions for NVIC and SysTick (add-on to CMSIS functions) */
 
 /* Exported types ------------------------------------------------------------*/
 /* Exported constants --------------------------------------------------------*/
--- external/platform/stm32f0xx/STM32F0xx_StdPeriph_Driver/rules.mk
+++ external/platform/stm32f0xx/STM32F0xx_StdPeriph_Driver/rules.mk
@@ -5,29 +5,6 @@ MODULE := $(LOCAL_DIR)
 GLOBAL_INCLUDES += $(LOCAL_DIR)/inc
 
 MODULE_SRCS += \
-	$(LOCAL_DIR)/src/stm32f0xx_adc.c \
-	$(LOCAL_DIR)/src/stm32f0xx_can.c \
-	$(LOCAL_DIR)/src/stm32f0xx_cec.c \
-	$(LOCAL_DIR)/src/stm32f0xx_comp.c \
-	$(LOCAL_DIR)/src/stm32f0xx_crc.c \
-	$(LOCAL_DIR)/src/stm32f0xx_crs.c \
-	$(LOCAL_DIR)/src/stm32f0xx_dac.c \
-	$(LOCAL_DIR)/src/stm32f0xx_dbgmcu.c \
-	$(LOCAL_DIR)/src/stm32f0xx_dma.c \
-	$(LOCAL_DIR)/src/stm32f0xx_exti.c \
-	$(LOCAL_DIR)/src/stm32f0xx_flash.c \
-	$(LOCAL_DIR)/src/stm32f0xx_gpio.c \
-	$(LOCAL_DIR)/src/stm32f0xx_i2c.c \
-	$(LOCAL_DIR)/src/stm32f0xx_iwdg.c \
-	$(LOCAL_DIR)/src/stm32f0xx_misc.c \
-	$(LOCAL_DIR)/src/stm32f0xx_pwr.c \
-	$(LOCAL_DIR)/src/stm32f0xx_rcc.c \
-	$(LOCAL_DIR)/src/stm32f0xx_rtc.c \
-	$(LOCAL_DIR)/src/stm32f0xx_spi.c \
-	$(LOCAL_DIR)/src/stm32f0xx_syscfg.c \
-	$(LOCAL_DIR)/src/stm32f0xx_tim.c \
-	$(LOCAL_DIR)/src/stm32f0xx_usart.c \
-	$(LOCAL_DIR)/src/stm32f0xx_wwdg.c \
 	$(LOCAL_DIR)/src/system_stm32f0xx.c
 
 include $(LOCAL_DIR)/CMSIS/rules.mk
