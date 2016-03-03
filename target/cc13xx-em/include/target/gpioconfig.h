a7fdfbb0dc58cf7dcc9862b01847d0c4858579fb
Brian Swetland <swetland@playground.global>
2016-03-03 14:07:29 -0800

    [target][cc13xx-em] cc13xx evaluation module target

---
+++ target/cc13xx-em/include/target/gpioconfig.h
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2016 Brian Swetland
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files
+ * (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge,
+ * publish, distribute, sublicense, and/or sell copies of the Software,
+ * and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#pragma once
+
+#define GPIO_LED1	25
+#define GPIO_LED2	27
+#define GPIO_LED3	7
+#define GPIO_LED4	6
+
+#define GPIO_BTN_SELECT	11
+#define GPIO_BTN_DOWN	12
+#define GPIO_BTN_LEFT	15
+#define GPIO_BTN_RIGHT	18
+#define GPIO_BTN_UP	19
+
+#define GPIO_UART_TX	3
+#define GPIO_UART_RX	2
+
