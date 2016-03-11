7aa44cdf3ca4fdfc8adc8746fef7e3022f092522
Gurjant Kalsi <me@gurjantkalsi.com>
2016-03-11 15:59:40 -0800

    [SQUASH] Assign copyright to Google

--- tools/moot/mtldr.py
+++ tools/moot/mtldr.py
@@ -1,5 +1,6 @@
 """
- Copyright (c) 2016 Gurjant Kalsi <me@gurjantkalsi.com>
+ Copyright 2016 Google Inc. All Rights Reserved.
+ Author: gkalsi@google.com (Gurjant Kalsi)
 
  Permission is hereby granted, free of charge, to any person obtaining
  a copy of this software and associated documentation files
