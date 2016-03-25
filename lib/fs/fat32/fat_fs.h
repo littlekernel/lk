f4262165cfd4943e90e0497f7efb4e34b8907ff6
Steve White <stepwhite@gmail.com>
2016-03-25 12:53:10 -0700

    [fs][fat32] Code formatting (scripts/codestyle)

--- lib/fs/fat32/fat_fs.h
+++ lib/fs/fat32/fat_fs.h
@@ -28,42 +28,42 @@
 #include <lib/bcache.h>
 
 typedef struct {
-  bdev_t *dev;
-  bcache_t cache;
-  
-  uint32_t lba_start;
-  
-  uint32_t bytes_per_sector;
-  uint32_t sectors_per_cluster;
-  uint32_t bytes_per_cluster;
-  uint32_t reserved_sectors;
-  uint32_t fat_bits;
-  uint32_t fat_count;
-  uint32_t sectors_per_fat;
-  uint32_t total_sectors;
-  uint32_t active_fat;
-  uint32_t data_start;
-  uint32_t total_clusters;
-  uint32_t root_cluster;
-  uint32_t root_entries;
-  uint32_t root_start;
+	bdev_t *dev;
+	bcache_t cache;
+
+	uint32_t lba_start;
+
+	uint32_t bytes_per_sector;
+	uint32_t sectors_per_cluster;
+	uint32_t bytes_per_cluster;
+	uint32_t reserved_sectors;
+	uint32_t fat_bits;
+	uint32_t fat_count;
+	uint32_t sectors_per_fat;
+	uint32_t total_sectors;
+	uint32_t active_fat;
+	uint32_t data_start;
+	uint32_t total_clusters;
+	uint32_t root_cluster;
+	uint32_t root_entries;
+	uint32_t root_start;
 } fat_fs_t;
 
 typedef struct {
-  fat_fs_t *fat_fs;
-  uint32_t start_cluster;
-  uint32_t length;
-  uint8_t attributes;
+	fat_fs_t *fat_fs;
+	uint32_t start_cluster;
+	uint32_t length;
+	uint8_t attributes;
 } fat_file_t;
 
 typedef enum {
-  fat_attribute_read_only = 0x01,
-  fat_attribute_hidden = 0x02,
-  fat_attribute_system = 0x04,
-  fat_attribute_volume_id = 0x08,
-  fat_attribute_directory = 0x10,
-  fat_attribute_archive = 0x20,
-  fat_attribute_lfn = fat_attribute_read_only | fat_attribute_hidden | fat_attribute_system | fat_attribute_volume_id,
+	fat_attribute_read_only = 0x01,
+	fat_attribute_hidden = 0x02,
+	fat_attribute_system = 0x04,
+	fat_attribute_volume_id = 0x08,
+	fat_attribute_directory = 0x10,
+	fat_attribute_archive = 0x20,
+	fat_attribute_lfn = fat_attribute_read_only | fat_attribute_hidden | fat_attribute_system | fat_attribute_volume_id,
 } fat_attributes;
 
 #define fat_read32(buffer,off) \
--- lib/fs/fat32/rules.mk
+++ lib/fs/fat32/rules.mk
@@ -1,14 +1,16 @@
-LOCAL_DIR := $(GET_LOCAL_DIR)
+LOCAL_DIR :
+    = $(GET_LOCAL_DIR)
 
-MODULE := $(LOCAL_DIR)
+      MODULE :
+      = $(LOCAL_DIR)
 
-MODULE_DEPS += \
-	lib/fs \
-	lib/bcache \
-	lib/bio
+        MODULE_DEPS += \
+                       lib/fs \
+                       lib/bcache \
+                       lib/bio
 
-MODULE_SRCS += \
-	$(LOCAL_DIR)/fat.c \
-	$(LOCAL_DIR)/file.c
+                       MODULE_SRCS += \
+                                      $(LOCAL_DIR)/fat.c \
+                                      $(LOCAL_DIR)/file.c
 
-include make/module.mk
+                                      include make/module.mk
