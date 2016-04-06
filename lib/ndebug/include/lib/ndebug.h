4c32c6aca39ce20c9890b450ba32db2e11be5214
Gurjant Kalsi <me@gurjantkalsi.com>
2016-04-06 15:09:10 -0700

    [SQUASH] Autoformat ndebug

--- lib/ndebug/include/lib/ndebug.h
+++ lib/ndebug/include/lib/ndebug.h
@@ -4,10 +4,10 @@
 #include <sys/types.h>
 
 typedef enum {
-	NDEBUG_CHANNEL_SYS,
-	NDEBUG_CHANNEL_USR,
+    NDEBUG_CHANNEL_SYS,
+    NDEBUG_CHANNEL_USR,
 
-	NDEBUG_CHANNEL_COUNT,	// Count: always last.
+    NDEBUG_CHANNEL_COUNT,   // Count: always last.
 } channel_t;
 
 void ndebug_init(void);
--- lib/ndebug/ndebug.c
+++ lib/ndebug/ndebug.c
@@ -18,7 +18,7 @@
 
 #define CH_TO_ADDR(CH) ((CH) + 1)
 #define CHECK_CHANNEL(CH) \
-		do { DEBUG_ASSERT((CH) < NDEBUG_CHANNEL_COUNT); } while (false);
+        do { DEBUG_ASSERT((CH) < NDEBUG_CHANNEL_COUNT); } while (false);
 
 #define SYS_EP_ADDR (CH_TO_ADDR(NDEBUG_CHANNEL_SYS))
 #define USR_EP_ADDR (CH_TO_ADDR(NDEBUG_CHANNEL_USR))
@@ -61,26 +61,26 @@ static const uint8_t bulk_pair_descriptor_template[] = {
 
 static void init_channel(uint8_t subclass, uint8_t protocol, uint8_t ep_addr)
 {
-	uint8_t desc[sizeof(bulk_pair_descriptor_template)];
+    uint8_t desc[sizeof(bulk_pair_descriptor_template)];
 
-	// Make a copy of the template descriptor and fill in the missing fields.
-	memcpy(desc, bulk_pair_descriptor_template, 
-		   sizeof(bulk_pair_descriptor_template));
+    // Make a copy of the template descriptor and fill in the missing fields.
+    memcpy(desc, bulk_pair_descriptor_template,
+           sizeof(bulk_pair_descriptor_template));
 
-	desc[IFACE_SUBCLASS_IDX] = subclass;
-	desc[IFACE_PROTOCOL_IDX] = protocol;
-	desc[IFACE_IN_ADDR_IDX] = ep_addr | 0x80;
-	desc[IFACE_OUT_ADDR_IDX] = ep_addr;
+    desc[IFACE_SUBCLASS_IDX] = subclass;
+    desc[IFACE_PROTOCOL_IDX] = protocol;
+    desc[IFACE_IN_ADDR_IDX] = ep_addr | 0x80;
+    desc[IFACE_OUT_ADDR_IDX] = ep_addr;
 
-	// Append the interfaces.
-	usb_append_interface_lowspeed(desc, sizeof(desc));
+    // Append the interfaces.
+    usb_append_interface_lowspeed(desc, sizeof(desc));
     usb_append_interface_highspeed(desc, sizeof(desc));
 }
 
 static void setup_usb_endpoint(uint8_t ep_num)
 {
-	usbc_setup_endpoint(ep_num, USB_IN, 0x40, USB_BULK);
-	usbc_setup_endpoint(ep_num, USB_OUT, 0x40, USB_BULK);
+    usbc_setup_endpoint(ep_num, USB_IN, 0x40, USB_BULK);
+    usbc_setup_endpoint(ep_num, USB_OUT, 0x40, USB_BULK);
 }
 
 static status_t ndebug_register_cb(
@@ -91,29 +91,29 @@ static status_t ndebug_register_cb(
 {
     if (op == USB_CB_ONLINE) {
         for (channel_t ch = 0; ch < NDEBUG_CHANNEL_COUNT; ++ch) {
-    		setup_usb_endpoint(CH_TO_ADDR(ch));
-    	}
+            setup_usb_endpoint(CH_TO_ADDR(ch));
+        }
     }
     return NO_ERROR;
 }
 
 void ndebug_init(void)
 {
-	for (channel_t ch = 0; ch < NDEBUG_CHANNEL_COUNT; ++ch) {
-		event_init(&rx_event[ch], 0, EVENT_FLAG_AUTOUNSIGNAL);
-		event_init(&tx_event[ch], 0, EVENT_FLAG_AUTOUNSIGNAL);
+    for (channel_t ch = 0; ch < NDEBUG_CHANNEL_COUNT; ++ch) {
+        event_init(&rx_event[ch], 0, EVENT_FLAG_AUTOUNSIGNAL);
+        event_init(&tx_event[ch], 0, EVENT_FLAG_AUTOUNSIGNAL);
     }
 
     init_channel(NDEBUG_SUBCLASS, NDEBUG_PROTOCOL_LK_SYSTEM, SYS_EP_ADDR);
-	init_channel(NDEBUG_SUBCLASS, NDEBUG_PROTOCOL_SERIAL_PIPE, USR_EP_ADDR);
+    init_channel(NDEBUG_SUBCLASS, NDEBUG_PROTOCOL_SERIAL_PIPE, USR_EP_ADDR);
 
     usb_register_callback(&ndebug_register_cb, NULL);
 }
 
 static status_t usb_xmit_cplt_cb(ep_t endpoint, usbc_transfer_t *t)
 {
-	uint32_t channel = (uint32_t)t->extra;
-	CHECK_CHANNEL(channel);
+    uint32_t channel = (uint32_t)t->extra;
+    CHECK_CHANNEL(channel);
 
     event_signal(&tx_event[channel], false);
     return 0;
@@ -121,8 +121,8 @@ static status_t usb_xmit_cplt_cb(ep_t endpoint, usbc_transfer_t *t)
 
 static status_t usb_recv_cplt_cb(ep_t endpoint, usbc_transfer_t *t)
 {
-	uint32_t channel = (uint32_t)t->extra;
-	CHECK_CHANNEL(channel);
+    uint32_t channel = (uint32_t)t->extra;
+    CHECK_CHANNEL(channel);
 
     event_signal(&rx_event[channel], false);
     return 0;
@@ -130,22 +130,22 @@ static status_t usb_recv_cplt_cb(ep_t endpoint, usbc_transfer_t *t)
 
 ssize_t ndebug_read(const channel_t ch, const size_t n, uint8_t *buf)
 {
-	CHECK_CHANNEL(ch);
+    CHECK_CHANNEL(ch);
 
-	usbc_transfer_t transfer = {
-	    .callback = &usb_recv_cplt_cb,
-	    .result = 0,
-	    .buf = buf,
-	    .buflen = n,
-	    .bufpos = 0,
-	    .extra = (void *)ch,
-	};
+    usbc_transfer_t transfer = {
+        .callback = &usb_recv_cplt_cb,
+        .result = 0,
+        .buf = buf,
+        .buflen = n,
+        .bufpos = 0,
+        .extra = (void *)ch,
+    };
 
     usbc_queue_rx(CH_TO_ADDR(ch), &transfer);
     status_t res = event_wait(&rx_event[ch]);
 
     if (res != NO_ERROR) {
-    	return res;
+        return res;
     }
 
     return transfer.bufpos;
@@ -153,22 +153,22 @@ ssize_t ndebug_read(const channel_t ch, const size_t n, uint8_t *buf)
 
 ssize_t ndebug_write(const channel_t ch, const size_t n, uint8_t *buf)
 {
-	CHECK_CHANNEL(ch);
+    CHECK_CHANNEL(ch);
 
-	usbc_transfer_t transfer = {
-	    .callback = &usb_xmit_cplt_cb,
-	    .result = 0,
-	    .buf = buf,
-	    .buflen = n,
-	    .bufpos = 0,
-	    .extra = (void *)ch,
-	};
+    usbc_transfer_t transfer = {
+        .callback = &usb_xmit_cplt_cb,
+        .result = 0,
+        .buf = buf,
+        .buflen = n,
+        .bufpos = 0,
+        .extra = (void *)ch,
+    };
 
     usbc_queue_tx(CH_TO_ADDR(ch), &transfer);
     status_t res = event_wait(&tx_event[ch]);
 
     if (res != NO_ERROR) {
-    	return res;
+        return res;
     }
 
     return n;
