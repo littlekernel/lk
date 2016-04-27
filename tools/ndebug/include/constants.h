62cc5ee77afcc0a4fbc20a039bdcd43af95f7f81
Gurjant Kalsi <me@gurjantkalsi.com>
2016-04-26 17:35:37 -0700

    [ndebug] Factor constants out into a shared constants file.

---
+++ tools/ndebug/include/constants.h
@@ -0,0 +1,10 @@
+#pragma once
+
+namespace NDebug {
+namespace Constants {
+
+const uint16_t kVendorId = 0x18D1;
+const uint16_t kProductId = 0xA010;
+
+}  // namespace Constants
+}  // namespace NDebug
