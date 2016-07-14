100133dc38720e8ca00e9012f32c5d9ee2a00d1b
Gurjant Kalsi <me@gurjantkalsi.com>
2016-07-13 17:31:53 -0700

    [BCM28xx] Roll BCM2836 and BCM2837 into a single platform. Have both RPi2 and RPi3 use the new BCM28XX platform.

--- target/rpi2/rules.mk
+++ target/rpi2/rules.mk
@@ -3,7 +3,7 @@ LOCAL_DIR := $(GET_LOCAL_DIR)
 GLOBAL_INCLUDES += \
 	$(LOCAL_DIR)/include
 
-PLATFORM := bcm2835
+PLATFORM := bcm28xx
 
 #include make/module.mk
 
--- target/rpi3/rules.mk
+++ target/rpi3/rules.mk
@@ -3,7 +3,7 @@ LOCAL_DIR := $(GET_LOCAL_DIR)
 GLOBAL_INCLUDES += \
 	$(LOCAL_DIR)/include
 
-PLATFORM := bcm2837
+PLATFORM := bcm28xx
 
 #include make/module.mk
 
