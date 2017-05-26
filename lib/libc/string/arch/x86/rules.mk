090491e9611f9a2c49635a1dc2f3d52f69740153
Zhong,Fangjian <fangjian.zhong@intel.com>
2017-05-26 13:19:45 +0800

    [lib][libc] x86 architecture memcpy/memset/memcpy_s implementation
    1. Introduce x86 architecture memcpy/memset implementation;
    2. Introduce memcpy_s for secure concern;
    
    Signed-off-by: Zhong,Fangjian <fangjian.zhong@intel.com>

--- lib/libc/string/arch/x86/rules.mk
+++ lib/libc/string/arch/x86/rules.mk
@@ -1,11 +1,9 @@
 LOCAL_DIR := $(GET_LOCAL_DIR)
 
-ASM_STRING_OPS := #bcopy bzero memcpy memmove memset
+ifeq ($(SUBARCH), x86-64)
+include $(LOCAL_DIR)/64/rules.mk
+else
+include $(LOCAL_DIR)/32/rules.mk
+endif
 
-MODULE_SRCS += \
-	#$(LOCAL_DIR)/memcpy.S \
-	#$(LOCAL_DIR)/memset.S
-
-# filter out the C implementation
-C_STRING_OPS := $(filter-out $(ASM_STRING_OPS),$(C_STRING_OPS))
-
+LOCAL_DIR :=
