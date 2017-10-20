37ba7fdf21ce10ab76c4f5ef7dbbd6e194c4ee4d
Erik Gilling <konkers@google.com>
2017-10-20 11:52:18 -0700

    [stm32f0xx] Properly initialize clocks in platform_early_init().
    This got broken when moving to the new STM32F0xx_HAL_Driver.

--- platform/stm32f0xx/init.c
+++ platform/stm32f0xx/init.c
@@ -27,12 +27,94 @@
 #include <platform/stm32.h>
 #include <platform/dma.h>
 #include <arch/arm/cm.h>
+#include <stm32f0xx_hal.h>
 #include "system_stm32f0xx.h"
+
+#if !defined(USE_USB_CLKSOURCE_PLL) && !defined(USE_USB_CLKSOURCE_CRSHSI48)
+#define USE_USB_CLKSOURCE_PLL
+#endif
+
+static void SystemClock_Config(void)
+{
+    RCC_ClkInitTypeDef RCC_ClkInitStruct;
+    RCC_OscInitTypeDef RCC_OscInitStruct;
+    RCC_PeriphCLKInitTypeDef  PeriphClkInitStruct;
+
+#if defined (USE_USB_CLKSOURCE_CRSHSI48)
+    static RCC_CRSInitTypeDef RCC_CRSInitStruct;
+#endif
+
+#if defined (USE_USB_CLKSOURCE_CRSHSI48)
+    /* Enable HSI48 Oscillator to be used as system clock source */
+    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI48;
+    HAL_RCC_OscConfig(&RCC_OscInitStruct);
+
+    /* Select HSI48 as USB clock source */
+    PeriphClkInitStruct.PeriphClockSelection = RCC_PERIPHCLK_USB;
+    PeriphClkInitStruct.UsbClockSelection = RCC_USBCLKSOURCE_HSI48;
+    HAL_RCCEx_PeriphCLKConfig(&PeriphClkInitStruct);
+
+    /* Select HSI48 as system clock source and configure the HCLK and PCLK1
+       clock dividers */
+    RCC_ClkInitStruct.ClockType = (RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_PCLK1);
+    RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI48;
+    RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+    RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+    HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_1);
+
+    /*Configure the clock recovery system (CRS)**********************************/
+
+    /*Enable CRS Clock*/
+    __HAL_RCC_CRS_CLK_ENABLE();
+
+    /* Default Synchro Signal division factor (not divided) */
+    RCC_CRSInitStruct.Prescaler = RCC_CRS_SYNC_DIV1;
+
+    /* Set the SYNCSRC[1:0] bits according to CRS_Source value */
+    RCC_CRSInitStruct.Source = RCC_CRS_SYNC_SOURCE_USB;
+
+    /* HSI48 is synchronized with USB SOF at 1KHz rate */
+    RCC_CRSInitStruct.ReloadValue =  __HAL_RCC_CRS_CALCULATE_RELOADVALUE(48000000, 1000);
+    RCC_CRSInitStruct.ErrorLimitValue = RCC_CRS_ERRORLIMIT_DEFAULT;
+
+    /* Set the TRIM[5:0] to the default value*/
+    RCC_CRSInitStruct.HSI48CalibrationValue = 0x20;
+
+    /* Start automatic synchronization */
+    HAL_RCCEx_CRSConfig (&RCC_CRSInitStruct);
+#elif defined (USE_USB_CLKSOURCE_PLL)
+    /* This code assumes that there is an 8MHz crystal. */
+
+    /* Enable HSE Oscillator and activate PLL with HSE as source
+       PLLCLK = (8 * 6) / 1) = 48 MHz */
+    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+    RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+    RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+    RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+    RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL6;
+    RCC_OscInitStruct.PLL.PREDIV = RCC_PREDIV_DIV1;
+    HAL_RCC_OscConfig(&RCC_OscInitStruct);
+
+    /*Select PLL 48 MHz output as USB clock source */
+    PeriphClkInitStruct.PeriphClockSelection = RCC_PERIPHCLK_USB;
+    PeriphClkInitStruct.UsbClockSelection = RCC_USBCLKSOURCE_PLLCLK;
+    HAL_RCCEx_PeriphCLKConfig(&PeriphClkInitStruct);
+
+    /* Select PLL as system clock source and configure the HCLK and PCLK1
+       clock dividers */
+    RCC_ClkInitStruct.ClockType = (RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_PCLK1);
+    RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+    RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+    RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+    HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_1);
+#endif /*USE_USB_CLKSOURCE_CRSHSI48*/
+}
 
 void platform_early_init(void)
 {
-    // Crank up the clock before initing timers.
+    HAL_Init();
     SystemInit();
+    SystemClock_Config();
 
     // start the systick timer
     // TODO(konkers): get sysclk freq from somewhere.
