4aaace9965bdc88ea4802e70ba35654fb30e85c7
Payam Moradshahi <payamm@google.com>
2018-03-22 14:09:36 -0700

    [arm64] Add support for chain_load for arm64 platforms

--- platform/amlogic-s912d/rules.mk
+++ platform/amlogic-s912d/rules.mk
@@ -9,21 +9,22 @@ MODULE_DEPS += \
     dev/timer/arm_generic \
 
 MODULE_SRCS += \
-	$(LOCAL_DIR)/platform.c \
-	$(LOCAL_DIR)/uart.c \
+    $(LOCAL_DIR)/platform.c \
+    $(LOCAL_DIR)/uart.c \
 
 ARCH := arm64
 ARM_CPU := cortex-a53
 MEMBASE := 0
 MEMSIZE := 0x80000000	# 2GB
-KERNEL_LOAD_OFFSET := 0x01080000
+KERNEL_LOAD_OFFSET := 0x01A80000
+
 LINKER_SCRIPT += \
-	$(BUILDDIR)/system-onesegment.ld
+    $(BUILDDIR)/system-onesegment.ld
 
 MODULE := $(LOCAL_DIR)
 
 MODULE_DEPS += \
-		app/shell \
+        app/shell \
 
 
 WITH_CPP_SUPPORT=true
