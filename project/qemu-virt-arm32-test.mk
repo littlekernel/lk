291bdce3645428598b7ca236360d8e69f33a4318
Travis Geiselbrecht <travisg@gmail.com>
2019-03-30 20:11:22 -0700

    [project][qemu-virt-arm] rename to arm32 and arm64
    Less confusing than a15 and a53

--- project/qemu-virt-arm32-test.mk
+++ project/qemu-virt-arm32-test.mk
@@ -1,9 +1,9 @@
-# main project for qemu-aarch64
+# main project for qemu-arm32
 MODULES += \
 	app/shell
 
 include project/virtual/test.mk
 include project/virtual/fs.mk
 include project/virtual/minip.mk
-include project/target/qemu-virt-a53.mk
+include project/target/qemu-virt-arm32.mk
 
--- project/qemu-virt-arm64-test.mk
+++ project/qemu-virt-arm64-test.mk
@@ -1,9 +1,9 @@
-# main project for qemu-arm32
+# main project for qemu-aarch64
 MODULES += \
 	app/shell
 
 include project/virtual/test.mk
 include project/virtual/fs.mk
 include project/virtual/minip.mk
-include project/target/qemu-virt-a15.mk
+include project/target/qemu-virt-arm64.mk
 
