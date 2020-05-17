7d9f80974d3a71d53f841197326c5b37a8094283
Travis Geiselbrecht <travisg@gmail.com>
2020-05-16 18:54:44 -0700

    [make][flags] remove -finline, which apparently does nothing
    Been carrying this flag around for years but from sleuthing around in
    the compiler it seems to only exist as the opposite to -fno-inline which
    has an actual effect. Only reason -finline would do anything would be to
    cancel a previous -fno-inline switch.

--- engine.mk
+++ engine.mk
@@ -55,7 +55,7 @@ CONFIGHEADER := $(BUILDDIR)/config.h
 
 GLOBAL_INCLUDES := $(BUILDDIR) $(addsuffix /include,$(LKINC))
 GLOBAL_OPTFLAGS ?= $(ARCH_OPTFLAGS)
-GLOBAL_COMPILEFLAGS := -g -finline -include $(CONFIGHEADER)
+GLOBAL_COMPILEFLAGS := -g -include $(CONFIGHEADER)
 GLOBAL_COMPILEFLAGS += -W -Wall -Wno-multichar -Wno-unused-parameter -Wno-unused-function -Wno-unused-label -Werror=return-type -Wno-nonnull-compare
 GLOBAL_COMPILEFLAGS += -fno-common
 GLOBAL_CFLAGS := --std=gnu11 -Werror-implicit-function-declaration -Wstrict-prototypes -Wwrite-strings
