f7c233a064b28bdc1c659cc47b100b4d593ce342
Logan Gorence <logan@gorence.io>
2020-05-17 12:07:10 -0700

    Fix incompatibility with Make 4.3.
    += does not prepend a space if the variable was empty.
    Source: https://lwn.net/Articles/810071/

--- make/macros.mk
+++ make/macros.mk
@@ -11,8 +11,8 @@ TOBUILDDIR = $(addprefix $(BUILDDIR)/,$(1))
 TOBOOL = $(if $(filter-out 0 false,$1),true,false)
 
 COMMA := ,
-SPACE :=
-SPACE +=
+E :=
+SPACE := $E $E
 
 # lower case and upper case translation
 LC = $(subst A,a,$(subst B,b,$(subst C,c,$(subst D,d,$(subst E,e,$(subst F,f,$(subst G,g,$(subst H,h,$(subst I,i,$(subst J,j,$(subst K,k,$(subst L,l,$(subst M,m,$(subst N,n,$(subst O,o,$(subst P,p,$(subst Q,q,$(subst R,r,$(subst S,s,$(subst T,t,$(subst U,u,$(subst V,v,$(subst W,w,$(subst X,x,$(subst Y,y,$(subst Z,z,$1))))))))))))))))))))))))))
