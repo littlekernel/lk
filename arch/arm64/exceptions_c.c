63b6d95cc92dce075558d41683b18defa600cbdc
Marco Nelissen <marcone@google.com>
2021-02-01 14:44:22 -0800

    [arch][arm64] Parse BRK exception
    When a crash is because of a BRK instruction, print that instead of
    the default "unhandled synchronous exception".
    
    Bug: 179516283
    
    
    Change-Id: I9667d7157d24a79e2b2ceb7ef283ebc2b09398d0

--- arch/arm64/exceptions_c.c
+++ arch/arm64/exceptions_c.c
@@ -204,6 +204,11 @@ void arm64_sync_exception(struct arm64_iframe_long *iframe) {
             print_fault_msg(BITS(iss, 5, 0));
             break;
         }
+        case 0b111100: {
+            printf("BRK #0x%04lx instruction: PC at 0x%llx(0x%lx)\n",
+                   BITS_SHIFT(iss, 15, 0), iframe->elr, display_pc);
+            break;
+        }
         default:
             printf("unhandled synchronous exception\n");
     }
