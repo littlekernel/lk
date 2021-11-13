5b1041748e8615883b52da78ead34817ccd1d4a8
Travis Geiselbrecht <travisg@gmail.com>
2021-11-12 20:40:41 -0800

    [libc] flesh out inttypes.h a bit more

--- lib/libc/include/inttypes.h
+++ lib/libc/include/inttypes.h
@@ -7,9 +7,40 @@
  */
 #pragma once
 
+#define PRId8 "hhd"
+#define PRIi8 "hhi"
+#define PRIo8 "hho"
+#define PRIu8 "hhu"
+#define PRIx8 "hhx"
+#define PRIX8 "hhX"
+
+#define PRId16 "hd"
+#define PRIi16 "hi"
+#define PRIo16 "ho"
+#define PRIu16 "hu"
+#define PRIx16 "hx"
+#define PRIX16 "hX"
+
+#define PRId32 "d"
+#define PRIi32 "i"
+#define PRIo32 "o"
 #define PRIu32 "u"
 #define PRIx32 "x"
+#define PRIX32 "X"
+
+#define PRId64 "lld"
+#define PRIi64 "lli"
+#define PRIo64 "llo"
+#define PRIu64 "llu"
+#define PRIx64 "llx"
+#define PRIX64 "llX"
+
+#define PRIdPTR "ld"
+#define PRIiPTR "li"
+#define PRIoPTR "lo"
+#define PRIuPTR "lu"
 #define PRIxPTR "lx"
+#define PRIXPTR "lX"
 
 #include <stdint.h>
 
