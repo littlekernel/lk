0679f91b9a9361c55b44db22d08963c1f1dce9a6
Travis Geiselbrecht <travisg@gmail.com>
2022-02-06 16:30:11 -0800

    [dev][net][e1000] only build with platforms with the VM enabled

--- dev/net/e1000/rules.mk
+++ dev/net/e1000/rules.mk
@@ -1,5 +1,8 @@
 LOCAL_DIR := $(GET_LOCAL_DIR)
 
+# At the moment, this can only be built with hardware MMU available.
+ifeq (true,$(call TOBOOL,$(WITH_KERNEL_VM)))
+
 MODULE := $(LOCAL_DIR)
 
 MODULE_SRCS += $(LOCAL_DIR)/e1000.cpp
@@ -8,3 +11,5 @@ MODULE_DEPS += dev/bus/pci
 MODULE_DEPS += lib/minip
 
 include make/module.mk
+
+endif # WITH_KERNEL_VM
