39fd0128636bd4dac9f9d8192e327ee20b62f98c
Chieh-Min Wang <cmwang@google.com>
2022-08-04 13:07:32 +0800

    [lib][stdio] wrap file io APIs with stdio APIs
    Implement stdio file io APIs with LK lib/fs APIs
    
    Signed-off-by: Chieh-Min Wang <cmwang@google.com>

--- lib/fs/test.c
+++ lib/fs/test.c
@@ -6,6 +6,7 @@
  * https://opensource.org/licenses/MIT
  */
 #include <lib/fs.h>
+#include <lk/err.h>
 
 #include <string.h>
 #include <string.h>
@@ -57,8 +58,56 @@ static bool test_path_normalize(void) {
     END_TEST;
 }
 
+#define TEST_MNT "/test"
+#define TEST_FILE TEST_MNT "/stdio_file_tests.txt"
+
+static inline void test_stdio_fs_teardown(void *ptr) {
+    fs_remove_file(TEST_FILE);
+    fs_unmount(TEST_MNT);
+}
+
+static bool test_stdio_fs(void) {
+    __attribute__((cleanup(test_stdio_fs_teardown))) BEGIN_TEST;
+
+    // Setup
+    const char *content = "Hello World\n";
+    const size_t content_len = strlen(content);
+    fs_mount(TEST_MNT, "memfs", NULL);
+
+    // Tests
+    FILE *stream = fopen(TEST_FILE, "w");
+    ASSERT_NE(NULL, stream, "failed to open/create file " TEST_MNT);
+
+    char buf[1024];
+    // test stdout fprintf, fputs
+    EXPECT_EQ(content_len, fprintf(stdout, "%s", content), "");
+    EXPECT_EQ(content_len, fputs(content, stdout), "");
+
+    // test fwrite
+    EXPECT_EQ(content_len, fwrite(content, 1, content_len, stream), "");
+
+    // test fread
+    ASSERT_EQ(NO_ERROR, fseek(stream, 0, SEEK_SET), "fseek failed");
+    EXPECT_EQ(content_len, fread(buf, 1, content_len, stream), "");
+    EXPECT_BYTES_EQ((const uint8_t *)content, (const uint8_t *)buf, content_len,
+                    "fread content mismatched");
+
+    // testing fputs
+    ASSERT_EQ(NO_ERROR, fseek(stream, 0, SEEK_SET), "fseek failed");
+    EXPECT_EQ(content_len, fputs(content, stream), "");
+
+    // testing fgets
+    ASSERT_EQ(NO_ERROR, fseek(stream, 0, SEEK_SET), "fseek failed");
+    ASSERT_NE(NULL, fgets(buf, content_len + 1, stream), "");
+    EXPECT_BYTES_EQ((const uint8_t *)content, (const uint8_t *)buf, content_len,
+                    "fgets content mismatched");
+
+    END_TEST;
+}
+
 BEGIN_TEST_CASE(fs_tests);
 RUN_TEST(test_path_normalize);
+RUN_TEST(test_stdio_fs);
 END_TEST_CASE(fs_tests);
 
 #endif // WITH_LIB_UNITTEST
