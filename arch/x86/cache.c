60bee016210bba0d5b0ebd9c4f05f11176006137
Travis Geiselbrecht <geist@foobox.com>
2024-05-09 19:54:54 -0700

    [arch][x86] stub out the cache routines

--- arch/x86/cache.c
+++ arch/x86/cache.c
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2009 Corey Tabaka
+ * Copyright (c) 2024 Travis Geiselbrecht
  *
  * Use of this source code is governed by a MIT-style
  * license that can be found in the LICENSE file or at
@@ -9,4 +10,14 @@
 
 /* nothing to do to sync I & D cache on x86 */
 void arch_sync_cache_range(addr_t start, size_t len) {
+}
+
+/* for the moment, no cache flushes here either */
+void arch_clean_cache_range(addr_t start, size_t len) {
+}
+
+void arch_clean_invalidate_cache_range(addr_t start, size_t len) {
+}
+
+void arch_invalidate_cache_range(addr_t start, size_t len) {
 }
