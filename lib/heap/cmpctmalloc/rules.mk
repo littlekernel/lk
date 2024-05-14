ed3876dae76b8bc0bacce14fd70eb9532072adf0
Travis Geiselbrecht <geist@foobox.com>
2024-05-14 00:47:43 -0700

    [lib][cmpctmalloc] mark cmpctmalloc as needing float
    It's just a test routine that's using floating point, but since nothing
    in cmpctmalloc should be used in interrupt context, it should be okay to
    mark this code as using float.

--- lib/heap/cmpctmalloc/rules.mk
+++ lib/heap/cmpctmalloc/rules.mk
@@ -5,4 +5,8 @@ MODULE := $(LOCAL_DIR)
 MODULE_SRCS += \
 	$(LOCAL_DIR)/cmpctmalloc.c
 
+# cmpct_test_trim() uses some floating point
+# TODO: move tests to another file to avoid this
+MODULE_OPTIONS := float
+
 include make/module.mk
