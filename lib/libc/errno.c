bb73c2296f79960f58cf765ccf94b90722976593
Matt Schulte <matsch@google.com>
2025-07-10 14:07:35 -0700

    [lib][libc] Make errno thread safe using TLS APIs
    Before this change, errno was "completely un-threadsafe" as the comment
    states.
    
    This changes errno to be threadsafe by making errno a thread local
    variable.

--- lib/libc/errno.c
+++ lib/libc/errno.c
@@ -7,12 +7,9 @@
  */
 
 #include <errno.h>
-
-/* completely un-threadsafe implementation of errno */
-/* TODO: pull from kernel TLS or some other thread local storage */
-static int _errno;
+#include <kernel/thread.h>
 
 int *__geterrno(void) {
-    return &_errno;
+    return (int*)tls_get(TLS_ENTRY_ERRNO);
 }
 
