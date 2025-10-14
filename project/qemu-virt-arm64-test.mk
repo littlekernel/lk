b06339d73d171b2d185ce6b0de2ad11050501544
David Brown <david.brown@linaro.org>
2025-10-14 21:05:07 +0000

    [rust] Conditionalize rust support
    Instead of just always enabling Rust, only enable if it is specifically
    requested via the `USE_RUST` variable.
    
    Signed-off-by: David Brown <david.brown@linaro.org>

--- project/qemu-virt-arm64-test.mk
+++ project/qemu-virt-arm64-test.mk
@@ -3,11 +3,7 @@ MODULES += \
 	app/shell \
     lib/uefi \
 
-MODULE_DEPS := \
-	lib/rust_support
-
 include project/virtual/test.mk
 include project/virtual/fs.mk
 include project/virtual/minip.mk
 include project/target/qemu-virt-arm64.mk
-
