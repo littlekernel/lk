ec6b0fb6e211318c851ef09b343daeabb6190e55
little fairy <30365474+2544632b@users.noreply.github.com>
2025-11-27 06:07:25 +0800

    [libc][string][x86] Enable memcpy and memset in rules.mk

--- lib/libc/string/arch/x86/rules.mk
+++ lib/libc/string/arch/x86/rules.mk
@@ -1,10 +1,10 @@
 LOCAL_DIR := $(GET_LOCAL_DIR)
 
-ASM_STRING_OPS := #bcopy bzero memcpy memmove memset
+ASM_STRING_OPS := memcpy memset #bcopy bzero memmove
 
 MODULE_SRCS += \
-	#$(LOCAL_DIR)/memcpy.S \
-	#$(LOCAL_DIR)/memset.S
+	$(LOCAL_DIR)/memcpy.S \
+	$(LOCAL_DIR)/memset.S
 
 # filter out the C implementation
 C_STRING_OPS := $(filter-out $(ASM_STRING_OPS),$(C_STRING_OPS))
