ca0e3257b7edca792a103fceb3fe08d83ff07159
2544632b <30365474+2544632b@users.noreply.github.com>
2025-11-28 06:48:36 +0800

    Revert "[libc][string][arch] Fix include logic for architecture-specific rules"
    This reverts commit 1740ab740f8d7fcbd4f4fd7aa3547f8502be93e9.

--- lib/libc/string/rules.mk
+++ lib/libc/string/rules.mk
@@ -36,12 +36,7 @@ LIBC_STRING_C_DIR := $(LOCAL_DIR)
 # include the arch specific string routines
 #
 # the makefile may filter out implemented versions from the C_STRING_OPS variable
-# x86 or x86-64 or anothers .....? T_T
-ifeq ($(SUBARCH),)
-	-include $(LOCAL_DIR)/arch/$(SUBARCH)/rules.mk
-else
-	-include $(LOCAL_DIR)/arch/$(ARCH)/rules.mk
-endif
+-include $(LOCAL_DIR)/arch/$(ARCH)/rules.mk
 
 MODULE_SRCS += \
 	$(addprefix $(LIBC_STRING_C_DIR)/,$(addsuffix .c,$(C_STRING_OPS)))
