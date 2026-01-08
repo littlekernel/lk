85e93da13849487189ffc154b552a1942fd2550d
Travis Geiselbrecht <geist@foobox.com>
2026-01-07 21:58:40 -0800

    [m68k] print some more exception details for unhandled exceptions

--- arch/m68k/exceptions.c
+++ arch/m68k/exceptions.c
@@ -17,14 +17,19 @@
 typedef struct m68k_iframe {
     uint32_t d[8];
     uint32_t a[7];
+
+    // standard 4 word m68k interrupt frame (format 0)
     uint16_t sr;
     uint16_t pc_high;
     uint16_t pc_low;
     uint16_t format : 4;
     uint16_t vector_offset : 12;
+
+    // extended info based on format (2, 3, 4)
+    uint32_t extended_info[0];
 } m68k_iframe_t;
 
-void dump_iframe(const m68k_iframe_t *iframe) {
+static void dump_iframe(const m68k_iframe_t *iframe) {
     printf("pc 0x%08x sr 0x%04x format %#x vector %#x\n", iframe->pc_low | iframe->pc_high << 16, iframe->sr,
             iframe->format, iframe->vector_offset / 4);
     printf("d0 0x%08x d1 0x%08x d2 0x%08x d3 0x%08x\n", iframe->d[0], iframe->d[1], iframe->d[2], iframe->d[3]);
@@ -33,25 +38,57 @@ void dump_iframe(const m68k_iframe_t *iframe) {
     printf("a4 0x%08x a5 0x%08x a6 0x%08x\n", iframe->a[4], iframe->a[5], iframe->a[6]);
 }
 
+static const char *exception_name(uint8_t code) {
+    switch (code) {
+        case 0: return "reset";
+        case 2: return "access fault";
+        case 3: return "address error";
+        case 4: return "illegal instruction";
+        case 5: return "divide by zero";
+        case 6: return "CHK instruction";
+        case 7: return "TRAPV instruction";
+        case 8: return "privilege violation";
+        case 9: return "trace";
+        case 10: return "line 1010 emulator";
+        case 11: return "line 1111 emulator";
+        case 12: return "emulator interrupt"; // 060
+        case 13: return "coprocessor protocol violation"; // 030
+        case 14: return "format error";
+        case 15: return "uninitialized interrupt";
+
+        // 000 - 030
+        case 56: return "mmu configuration error";
+        case 57: return "mmu illegal operation";
+        case 58: return "mmu access violation";
+
+        // 060 specific
+        case 60: return "unimplemented effective address";
+        case 61: return "unimplemented instruction";
+
+        default: return "unknown";
+    }
+}
+
+// General exceptions from 2 - 15
 void m68k_exception(m68k_iframe_t *frame) {
     uint8_t code = frame->vector_offset / 4;
 
-    LTRACEF("frame %p, code %#hhx\n", frame, code);
+    LTRACEF("frame %p, code %#hhx (%s)\n", frame, code, exception_name(code));
 
     dump_iframe(frame);
 
     printf("more stack:\n");
     hexdump8(frame, 256);
 
-    panic("unimplemented exception %#hhx\n", code);
+    panic("unimplemented exception %#hhx (%s)\n", code, exception_name(code));
 }
 
 void m68k_trap_exception(m68k_iframe_t *frame) {
-    panic("unhandled trap exception!\n");
+    panic("unhandled trap exception\n");
 }
 
 void m68k_null_trap(m68k_iframe_t *frame) {
-    panic("unhandled null trap exception!");
+    panic("unhandled null trap exception");
 }
 
 // defined in platform interrupt controller
