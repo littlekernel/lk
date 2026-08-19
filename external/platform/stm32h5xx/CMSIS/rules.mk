839a320bbd3b93140c5917126184ddb620bd49e3
Travis Geiselbrecht <geist@foobox.com>
2026-08-19 01:21:01 -0700

    [external][stm32h5xx] declare the libm header dependency
    stm32h5xx.h includes math.h for the float_t that SystemCoreClockUpdate()
    uses, which scripts/check-module-deps.py (and so the gcc CI matrix, which
    runs buildall -d) reports as an undeclared edge.

--- external/platform/stm32h5xx/CMSIS/rules.mk
+++ external/platform/stm32h5xx/CMSIS/rules.mk
@@ -6,4 +6,7 @@ GLOBAL_INCLUDES += $(LOCAL_DIR)/inc
 
 MODULE_SRCS += $(LOCAL_DIR)/system_stm32h5xx.c
 
+# stm32h5xx.h includes math.h, which SystemCoreClockUpdate() uses for float_t
+MODULE_DEPS += lib/libm
+
 include make/module.mk
